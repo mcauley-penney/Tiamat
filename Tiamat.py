b9537a14fa9ea3944b88cf3b6a8707d5e85f9fc3
McAuley Penney <jacobmpenney@gmail.com>
2024-06-11 07:30:56 -0700

    chore: lint and clean src
    - add placeholder docstrings
    - reduce number of class instance attributes by demoting some attributes
      tolocal variables where possible
    - remove unnecessary comments and commented-out source code

--- Tiamat.py
+++ Tiamat.py
@@ -1,22 +1,9 @@
-# TODO:
-# 1. Init conversational ability
-#   - make it async, nonblocking
-# 2. messages must be tracked and used as context
-# 3. Panel should be responsive to main window
-#   - color
-#   - font
-#   - toggle panel visibility
-#
-# Path:
-# 1. connect agent
-# 2. work on conversational ability
+"""TODO."""
 
-
-import aiohttp
 import asyncio
-from concurrent.futures import Future
 import threading
 import tkinter as tk
+import aiohttp
 
 
 ENDPOINT = "http://127.0.0.1:8000/v1/assistant/prompt"
@@ -31,32 +18,34 @@ async def async_post(url, payload, headers):
 
 
 class Tiamat:
+    """TODO."""
+
     def __init__(self, editwin):
         self.editwin = editwin
         self.history = []
 
         self.async_loop = asyncio.new_event_loop()
-        self.thread = threading.Thread(target=self.start_loop, args=(self.async_loop,))
-        self.thread.start()
+
+        thread = threading.Thread(target=self.start_loop, args=(self.async_loop,))
+        thread.start()
 
         self.init_widgets()
 
-        # TODO: check health of assistant to make sure we may continue
-        #  self.check_assistant_health()
-
     def start_loop(self, loop):
+        """TODO."""
         asyncio.set_event_loop(loop)
         loop.run_forever()
 
     def init_widgets(self):
-        self.panel = tk.Frame(self.editwin.top, bg="white")
-        self.panel.pack(side="left", fill="y", expand=False, padx=(0, 0), pady=(0, 0))
+        """TODO."""
+        panel = tk.Frame(self.editwin.top, bg="white")
+        panel.pack(side="left", fill="y", expand=False, padx=(0, 0), pady=(0, 0))
 
-        self.feed_box = tk.Frame(self.panel, borderwidth=2, relief="sunken")
+        self.feed_box = tk.Frame(panel, borderwidth=2, relief="sunken")
         self.feed_box.pack(side="bottom", fill="x", padx=5, pady=5)
 
         self.msgfeed = tk.Text(
-            self.panel,
+            panel,
             state="disabled",
             height=20,
             width=50,
@@ -71,7 +60,7 @@ class Tiamat:
         self.input_box.pack(side="left", fill="both", expand=True, padx=0, pady=1)
         self.input_box.bind("<Return>", self.handle_user_input)
 
-        self.submit_btn = tk.Button(
+        submit_btn = tk.Button(
             self.feed_box,
             command=self.handle_user_input,
             text="",
@@ -85,9 +74,10 @@ class Tiamat:
             relief=tk.FLAT,
             font=("Arial", 30, "bold"),
         )
-        self.submit_btn.pack(side="right", padx=0, pady=1)
+        submit_btn.pack(side="right", padx=0, pady=1)
 
     def handle_user_input(self, event=None):
+        """TODO."""
         user_input = self.input_box.get("1.0", tk.END).strip()
         self.input_box.delete("1.0", tk.END)
 
@@ -100,6 +90,7 @@ class Tiamat:
         return "break"
 
     async def query_assistant(self, msg):
+        """TODO."""
         payload = {"message": msg, "session_id": "IDLE"}
         headers = {"Content-Type": "application/json"}
 
@@ -109,18 +100,12 @@ class Tiamat:
         self.print_msg("You", msg)
 
         response = await post_task
-
         answer: str = response["answer"]
-        # check the response (optional)
-        if answer is not None:
-            print("Success:", answer)
-            return answer
-        else:
-            print("An error occurred:", None)
-            return None
 
+        return answer
 
     def handle_result(self, future):
+        """TODO."""
         response = future.result()
 
         if response is None:
@@ -130,6 +115,7 @@ class Tiamat:
             self.print_msg("Assistant", response)
 
     def print_msg(self, speaker, msg):
+        """TODO."""
         msg = f"{speaker}\n{msg}\n\n"
         print(msg)
 
@@ -138,33 +124,3 @@ class Tiamat:
 
         self.msgfeed.config(state="disabled")
         self.msgfeed.see(tk.END)
-
-
-#  from idlelib.config import idleConf
-
-#  def show_sidebar(self):
-#      if not self.is_shown:
-#          self.grid()
-#          self.is_shown = True
-
-#  def hide_sidebar(self):
-#      if self.is_shown:
-#          self.main_widget.grid_forget()
-#          self.is_shown = False
-
-#  def update_font(self):
-#      font = idleConf.GetFont(self.text, "main", "EditorWindow")
-#      self.sidebar_text["font"] = font
-
-#  def update_colors(self):
-#      """Update the sidebar text colors, usually after config changes."""
-#      colors = idleConf.GetHighlight(idleConf.CurrentTheme(), "linenumber")
-#      foreground = colors["foreground"]
-#      background = colors["background"]
-#      self.sidebar_text.config(
-#          fg=foreground,
-#          bg=background,
-#          selectforeground=foreground,
-#          selectbackground=background,
-#          inactiveselectbackground=background,
-#      )
